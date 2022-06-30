644ef8fe04e3cce18155442bd11865969fc087e7
BambooSky <bamboosky2021@163.com>
2022-06-30 16:14:23 +0800

    first commit

---
+++ Input_Output/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("输入数字：");
+        int n = input.nextInt();
+        System.out.println(n);
+
+        System.out.print("输入字符1:");
+        String s = input.next();
+        System.out.println(s);
+
+        System.out.print("输入字符2:");
+
+        String ss = input.next();
+        System.out.println(ss);
+    }
+}
